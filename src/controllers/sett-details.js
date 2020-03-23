0cf6b7f9d2589172e23b2ced7abab728613f57ac
Mike Coats <mike.coats@nature.scot>
2020-03-23 16:41:26 +0000

    Ensure all grid refs are 8 digit or longer
    Issue: Scottish-Natural-Heritage/Licensing#203

--- src/controllers/sett-details.js
+++ src/controllers/sett-details.js
@@ -12,24 +12,46 @@ const validSettId = (settId) => {
   return true;
 };
 
-const validGridReference = (gridReference) => {
-  if (gridReference === undefined) {
+/**
+ * Clean a string to remove any non-grid-ref characters.
+ *
+ * Takes something like '-NH_6400 4800__' and returns 'NH64004800'.
+ *
+ * @param {string} gridRef A user supplied grid ref of dubious quality.
+ * @returns {string} A nice tidy version of the grid ref.
+ */
+const formatGridReference = (gridRef) => {
+  return gridRef.toUpperCase().replace(/[^A-Z0-9]/g, '');
+};
+
+/**
+ * Check to see if the user supplied string looks like a grid ref.
+ *
+ * We first tidy up the user input, so that it's close to being a grid ref,
+ * then we check that what we have left is actually a grid ref.
+ *
+ * @param {string} gridRef A candidate grid ref.
+ * @returns {boolean} True if this looks like a valid grid ref, otherwise false.
+ */
+const validGridReference = (gridRef) => {
+  // Check to make sure we've got some input before we go any further.
+  if (gridRef === undefined) {
     return false;
   }
 
-  if (gridReference.trim() === '') {
+  // Tidy up the grid ref so that it's likely to pass validation.
+  const formattedGridRef = formatGridReference(gridRef);
+
+  // Later, we'll check that it's in the AA00000000 style, but we'll only be
+  // checking for 8 or more digits, not an even number of digits, so we need
+  // this one extra check.
+  if (formattedGridRef.length % 2 !== 0) {
     return false;
   }
 
-  if (gridReference.trim().length % 2 !== 0) {
-    return false;
-  }
-
-  if (gridReference.trim().length < 8) {
-    return false;
-  }
-
-  return true;
+  // Check that the gridRef is in the AA00000000 format, and fail them if
+  // it's not.
+  return /^[A-Z]{2}\d{8,}$/g.test(formattedGridRef);
 };
 
 const validSettType = (settType) => {
@@ -81,6 +103,7 @@ const settDetailsController = (req) => {
   if (req.session.settDetailsError) {
     req.session.currentSettId = req.body.currentSettId.trim();
     req.session.currentSettType = Number.parseInt(req.body.currentSettType, 10);
+    // Don't return the 'formatted' one here, just send back the original one. It's too confusing otherwise.
     req.session.currentGridReference = req.body.currentGridReference.trim();
     req.session.currentEntrances = req.body.currentEntrances;
 
@@ -91,7 +114,7 @@ const settDetailsController = (req) => {
     const newSett = {
       id: req.body.currentSettId.trim(),
       type: Number.parseInt(req.body.currentSettType, 10),
-      gridReference: req.body.currentGridReference.trim(),
+      gridReference: formatGridReference(req.body.currentGridReference),
       entrances: Number.parseInt(req.body.currentEntrances, 10)
     };
 
@@ -104,7 +127,7 @@ const settDetailsController = (req) => {
   } else {
     req.session.setts[req.session.currentSettIndex].id = req.body.currentSettId.trim();
     req.session.setts[req.session.currentSettIndex].type = Number.parseInt(req.body.currentSettType, 10);
-    req.session.setts[req.session.currentSettIndex].gridReference = req.body.currentGridReference.trim();
+    req.session.setts[req.session.currentSettIndex].gridReference = formatGridReference(req.body.currentGridReference);
     req.session.setts[req.session.currentSettIndex].entrances = Number.parseInt(req.body.currentEntrances, 10);
   }
 
