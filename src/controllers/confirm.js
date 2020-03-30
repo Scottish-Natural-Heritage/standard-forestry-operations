f246ec653ec322f0625cd0283d5eff64360eedb7
Mike Coats <mike.coats@nature.scot>
2020-03-30 15:51:24 +0100

    Create and update an application on the back-end
    Issue: Scottish-Natural-Heritage/Licensing#292

--- src/controllers/confirm.js
+++ src/controllers/confirm.js
@@ -1,8 +1,37 @@
+import axios from 'axios';
+import config from '../config.js';
 import {ReturnState} from './_base.js';
 
 const confirmController = async (req) => {
   try {
-    req.session.licenceNo = `NS-SFO-${Math.ceil(Math.random() * 99999)}`;
+    // Allocate a new application.
+    const newAppResponse = await axios.post(config.apiEndpoint);
+
+    // Determine where the back-end's saved it.
+    const newAppUrl = newAppResponse.headers.location;
+
+    // Get our application object ready for submission.
+    const newApp = {
+      convictions: req.session.conviction,
+      complyWithTerms: req.session.comply,
+      fullName: req.session.fullName,
+      companyOrganisation: req.session.companyOrganisation,
+      addressLine1: req.session.addressLine1,
+      addressLine2: req.session.addressLine2,
+      addressTown: req.session.addressTown,
+      addressCounty: req.session.addressCounty,
+      addressPostcode: req.session.addressPostcode,
+      phoneNumber: req.session.phoneNumber,
+      emailAddress: req.session.emailAddress,
+      siteName: req.session.siteName,
+      setts: req.session.setts
+    };
+
+    // Send the back-end our application.
+    const updatedAppResponse = await axios.put(newAppUrl, newApp);
+
+    // Save the licence details from the successful application.
+    req.session.licenceNo = `NS-SFO-${updatedAppResponse.data.id}`;
     req.session.expiryDate = `30/11/${new Date().getFullYear()}`;
 
     // Let them know it all went well.
