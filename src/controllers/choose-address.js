746399ee3926cea741ed03d6df4773746813b912
John Moore <john.moore@snh.gov.uk>
2022-04-20 15:17:49 +0100

    pushed bas skeleton app in

--- src/controllers/choose-address.js
+++ src/controllers/choose-address.js
@@ -1,32 +1,33 @@
 import {ReturnState} from './_base.js';
 
-const otherController = (request) => {
-  // Did the user tell us they're self applying.
-  if (request.body.other === 'self') {
+const chooseAddressController = (request) => {
+  // Did the user tell us they have no convictions.
+  if (request.body.conviction === 'no') {
     // Then we don't have any errors. This clears any previous errors.
-    request.session.otherError = false;
+    request.session.convictionError = false;
     // Save the decision.
-    request.session.other = false;
+    request.session.conviction = false;
     // Follow the 'happy path'.
     return ReturnState.Positive;
   }
 
-  // Did the user tell us they're applying for another.
-  if (request.body.other === 'other') {
+  // Did the user tell us they're have some convictions.
+  if (request.body.conviction === 'yes') {
     // It's a silly answer, but not an error. This clears any previous errors.
-    request.session.otherError = false;
+    request.session.convictionError = false;
     // Save the decision.
-    request.session.other = true;
+    request.session.conviction = true;
     // Go down the 'STOP' path.
     return ReturnState.Negative;
   }
 
   // The user submitted the form without selecting an option, this is an error!
-  request.session.otherError = true;
+  request.session.convictionError = true;
   // Unset any saved value.
-  request.session.other = undefined;
+  request.session.conviction = undefined;
   // Reload the page to highlight errors.
   return ReturnState.Error;
 };
 
-export {otherController as default};
+export {chooseAddressController as default};
+
---
+++ src/controllers/postcode.js
@@ -0,0 +1,9 @@
+import {ReturnState} from './_base.js';
+
+const postcodeController = (request) => {
+  // Much like the start page, the only way out of the before you start page is onwards,
+  // so return success and continue the form.
+  return ReturnState.Positive;
+};
+
+export {postcodeController as default};
