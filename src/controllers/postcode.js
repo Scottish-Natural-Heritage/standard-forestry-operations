82137c1289fb5a6fed3e93876ebd86bbebc8212c
murrayholmes96 <murray.holmes@nature.scot>
2022-05-03 14:15:08 +0100

    add logic to validate postcode
    Issue: Scottish-Natural-Heritage/Licensing#1203

--- src/controllers/postcode.js
+++ src/controllers/postcode.js
@@ -1,4 +1,5 @@
 import {ReturnState} from './_base.js';
+import utils from 'naturescot-utils';
 
 /**
  * Clean the incoming postcode to make it more compatible with the
@@ -9,22 +10,35 @@ import {ReturnState} from './_base.js';
  */
 const cleanInput = (body) =>{
   return{
-  addressPostcode: model.addressPostcode === undefined ? undefined : model.addressPostcodeostcode.trim()
+  addressPostcode: body.addressPostcode === undefined ? undefined : body.addressPostcode.trim()
   };
 };
 
 const postcodeController = () => {
+  // Clear errors.
+  request.session.postcodeError = false;
+  request.session.invalidPostcodeError = false;
+  request.session.emptyPostcodeError = false;
 
   const cleanForm = cleanInput(request.body);
   request.session.addressPostcode = cleanForm.addressPostcode;
-  request.session.addressPostcodeError = false;
 
-    // Call natureScot utils to check validity of postcode
-    request.session.postcodeError =
-    request.body.addressPostcode === undefined
-      ? true
-      : !utils.postalAddress.isaRealUkPostcode(request.body.addressPostcode);
+  request.session.emptyPostcodeError = request.session.addressPostcode === undefined || request.session.addressPostcode.trim() === '';
 
+  // Call natureScot utils to check validity of postcode
+  request.session.invalidPostcodeError = !utils.postalAddress.isaRealUkPostcode(request.session.addressPostcode);
+
+  // Check that any of the fields are invalid.
+  request.session.postcodeError = request.session.emptyPostcodeError || request.session.invalidPostcodeError;
+
+  // If we've seen an error in any of the fields, our visitor needs to go back
+  // and fix them.
+  if (request.session.postcodeError) {
+    return ReturnState.Error;
+  }
+
+  // The request passed all our validation, we've stored copies of everything we
+  // need, so it's time to go on.
   return ReturnState.Positive;
 };
 
