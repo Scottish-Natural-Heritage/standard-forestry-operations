2f4a5f093875c7f8f0a3a2c6bc2d47ef2874a04d
murrayholmes96 <murray.holmes@nature.scot>
2022-05-05 13:13:09 +0100

    added secure back to config to clean up johns mess
    Issue: Scottish-Natural-Heritage/Licensing#1203

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const config = Object.freeze({
   apiEndpoint: process.env.SFO_API_URL || 'http://localhost:3003/standard-forestry-operations-api/v1',
   hostPrefix: process.env.SFO_HOST_PREFIX || `http://localhost:${process.env.SFO_PORT}`,
   pathPrefix: process.env.SFO_PATH_PREFIX ? `/${process.env.SFO_PATH_PREFIX}` : '/standard-forestry-operations',
-  cookiePrefix: process.env.COOKIE_PREFIX || '__'
+  cookiePrefix: process.env.COOKIE_PREFIX || '__Secure'
 });
 
 export {config as default};
