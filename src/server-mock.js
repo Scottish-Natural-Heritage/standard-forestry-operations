5a695d62d8e93e99588531afb9aa7ac8467ef17e
John Moore <john.moore@snh.gov.uk>
2022-05-17 14:33:15 +0100

    Fix lint errors
    Issue: Scottish-Natural-Heritage/Licensing#1212

--- src/server-mock.js
+++ src/server-mock.js
@@ -8,174 +8,176 @@ import app from './app.js';
 
 const mockAxios = new MockAdapter(axios);
 
-mockAxios
-  .onGet('http://mock-gazetteer-api/endpoint')
-  .reply((config) => {
-    if (config.params.postcode && config.params.postcode === 'IV3 8NW') {
-      return [
-        200,
-        {
-          results: [
-            {
-              address: [
-                {
-                  uprn: 130_053_286,
-                  summary_address: 'Gateside, 1A Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_287,
-                  summary_address: 'Orcadia, 1B Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_284,
-                  summary_address: '2 Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_285,
-                  summary_address: '4 Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_278,
-                  summary_address: 'Burnside, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_279,
-                  summary_address: 'Crisdon, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 10_091_962_936,
-                  summary_address:
-                    'Crofting Commission, Crofting Commission, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 10_092_032_547,
-                  summary_address: 'Naturescot, Naturescot, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 10_092_024_846,
-                  summary_address:
-                    'Care Inspectorate, Care Inspectorate, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 10_091_801_055,
-                  summary_address:
-                    'Bord Na Gaidhlig, Bord Na Gaidhlig, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 10_091_801_051,
-                  summary_address:
-                    'Moray Firth Partnership, Moray Firth Partnership, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_137_528,
-                  summary_address: 'Visitscotland, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 10_091_801_052,
-                  summary_address:
-                    'Deer Commission For Scotland, Deer Commission For Scotland, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_280,
-                  summary_address: 'Hillside House, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_143_180,
-                  summary_address: 'Old Post Office, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_283,
-                  summary_address: 'The Old School, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_281,
-                  summary_address: 'The Brackens, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-                {
-                  uprn: 130_053_277,
-                  summary_address: 'Tigh Na Bruaich, Leachkin Road, Inverness, IV3 8NW',
-                  matchscore: 100,
-                },
-              ],
-            },
-          ],
-          metadata: {
-            querytime: 0.018_79,
-            vintage: 87,
-            jobid: null,
-            count: 18,
-            maxResults: 1000,
-            status: 'OK',
+/* Turn off spell checking for the place names. spell-checker:disable */
+/* eslint-disable camelcase */
+// prettier-ignore
+mockAxios.onGet('http://mock-gazetteer-api/endpoint').reply((config) => {
+  if (config.params.postcode && config.params.postcode === 'IV3 8NW') {
+    return [
+      200,
+      {
+        results: [
+          {
+            address: [
+              {
+                uprn: 130_053_286,
+                summary_address: 'Gateside, 1A Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_287,
+                summary_address: 'Orcadia, 1B Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_284,
+                summary_address: '2 Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_285,
+                summary_address: '4 Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_278,
+                summary_address: 'Burnside, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_279,
+                summary_address: 'Crisdon, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 10_091_962_936,
+                summary_address:
+                  'Crofting Commission, Crofting Commission, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 10_092_032_547,
+                summary_address: 'Naturescot, Naturescot, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 10_092_024_846,
+                summary_address:
+                  'Care Inspectorate, Care Inspectorate, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 10_091_801_055,
+                summary_address:
+                  'Bord Na Gaidhlig, Bord Na Gaidhlig, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 10_091_801_051,
+                summary_address:
+                  'Moray Firth Partnership, Moray Firth Partnership, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_137_528,
+                summary_address: 'Visitscotland, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 10_091_801_052,
+                summary_address:
+                  'Deer Commission For Scotland, Deer Commission For Scotland, Great Glen House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_280,
+                summary_address: 'Hillside House, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_143_180,
+                summary_address: 'Old Post Office, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_283,
+                summary_address: 'The Old School, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_281,
+                summary_address: 'The Brackens, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+              {
+                uprn: 130_053_277,
+                summary_address: 'Tigh Na Bruaich, Leachkin Road, Inverness, IV3 8NW',
+                matchscore: 100,
+              },
+            ],
           },
+        ],
+        metadata: {
+          querytime: 0.018_79,
+          vintage: 87,
+          jobid: null,
+          count: 18,
+          maxResults: 1000,
+          status: 'OK',
         },
-      ];
-    }
+      },
+    ];
+  }
 
-    if (config.params.postcode && config.params.postcode === 'XM4 5HQ') {
-      return [
-        200,
-        {
-          results: ['No records found.'],
-          metadata: {
-            querytime: 0.016_75,
-            vintage: 87,
-            jobid: null,
-            count: 0,
-            maxResults: 1000,
-            status: 'OK',
+  // prettier-ignore
+  if (config.params.postcode && config.params.postcode === 'XM4 5HQ') {
+    return [
+      200,
+      {
+        results: ['No records found.'],
+        metadata: {
+          querytime: 0.016_75,
+          vintage: 87,
+          jobid: null,
+          count: 0,
+          maxResults: 1000,
+          status: 'OK',
+        },
+      },
+    ];
+  }
+
+  // prettier-ignore
+  if (config.params.postcode && config.params.postcode === 'PH1 3EW') {
+    return [
+      200,
+      {
+        results: [
+          {
+            address: [
+              {
+                uprn: 124_062_288,
+                summary_address:
+                  'Scottish Natural Heritage, Battleby House, B8063 From The Entrance To Coldrochie Farm To The A9t Slip Road West Of Luncarty, Redgorton, PH1 3EW',
+                matchscore: 100,
+              },
+            ],
           },
+        ],
+        metadata: {
+          querytime: 0.017_08,
+          vintage: 87,
+          jobid: null,
+          count: 1,
+          maxResults: 1000,
+          status: 'OK',
         },
-      ];
-    }
+      },
+    ];
+  }
 
-    if (config.params.postcode && config.params.postcode === 'PH1 3EW') {
-      return [
-        200,
-        {
-          results: [
-            {
-              address: [
-                {
-                  uprn: 124_062_288,
-                  summary_address:
-                    'Scottish Natural Heritage, Battleby House, B8063 From The Entrance To Coldrochie Farm To The A9t Slip Road West Of Luncarty, Redgorton, PH1 3EW',
-                  matchscore: 100,
-                },
-              ],
-            },
-          ],
-          metadata: {
-            querytime: 0.017_08,
-            vintage: 87,
-            jobid: null,
-            count: 1,
-            maxResults: 1000,
-            status: 'OK',
-          },
-        },
-      ];
-    }
-
-    // prettier-ignore
-
-    if (
+  // prettier-ignore
+  if (
       (config.params.uprn && config.params.uprn === 10_092_032_547)) {
       return [
         200,
@@ -275,9 +277,11 @@ mockAxios
       ];
     }
 
-    return [500];
-  });
+  return [500];
+});
 
+/* Turn spell checking back on now. spell-checker:enable */
+/* eslint-enable camelcase */
 // Start the micro-app and log any errors.
 app.listen(config.port, () => {
   console.log(`Server listening on http://localhost:${config.port}${config.pathPrefix}.`);
